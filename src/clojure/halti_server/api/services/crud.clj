c67a3334b2d8b7bb82d5d10f8ade8823326fedeb
Teemu Heikkilä <teemu@emblica.fi>
2017-06-06 15:57:06 +0300

    Add extra_hosts as key

--- src/clojure/halti_server/api/services/crud.clj
+++ src/clojure/halti_server/api/services/crud.clj
@@ -27,6 +27,7 @@
    :service_id s/Str
    :image s/Str
    (s/optional-key :command) s/Str
+   (s/optional-key :extra_hosts) {s/Str s/Str}
    (s/optional-key :requirements) [s/Str]
    (s/optional-key :_id) s/Any
    :ports [Port]
@@ -39,6 +40,7 @@
                       :memory
                       :instances
                       :version
+                      :extra_hosts
                       :requirements
                       :image
                       :command
